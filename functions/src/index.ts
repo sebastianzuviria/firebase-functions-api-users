014445e9c01f4978e24987b20882e116c968b03b
Sebastian Zuviria <szuviria@gmail.com>
2022-06-19 03:06:40 -0300

    Update index.ts
    Example deleted

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,11 +14,3 @@ app.use(cors({ origin: true }));
 routesConfig(app);
 
 export const api = functions.https.onRequest(app);
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-});
